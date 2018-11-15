f8934311e19f67caf06cd438fbb41c514797d9cf
hifromkevin <lam.kevin86@gmail.com>
2018-11-14 16:18:12 -0800

    Adds MainMenu component

---
+++ client/src/components/MainMenu.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FontAwesome from 'react-fontawesome';
+
+const MainMenu = () => (
+  <div className="menu">
+		<h1 className="menu__logo"><FontAwesome name="file" /> NotGiphy</h1>
+  </div>
+);
+
+export default MainMenu;
