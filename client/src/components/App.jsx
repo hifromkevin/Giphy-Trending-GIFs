61b1e295734eac20d69e63aab5fcb6b62a2e277d
Kevin Lam <lam.kevin86@gmail.com>
2018-11-16 08:38:36 -0800

    Displays gifs from Giphy

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,23 +2,53 @@ import React, { Component } from 'react';
 import FontAwesome from 'react-fontawesome';
 
 import MainMenu from './MainMenu.jsx';
+import Search from './Search.jsx';
+import API_KEY from '../../../config/API_KEY.js';
 
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			abilities: ['Access to state', 'Utilize components', 'Use FontAwesome and SASS']
+			gifs: []
 		}
+	}
+
+	componentWillMount() {
+	fetch('http://api.giphy.com/v1/gifs/trending?api_key=OxyfaFK4scUWNYDQHaQDSoXC0GFWbddt&limit=5')
+		.then(response => response.json())
+		.then((response) => {
+			console.log('res', response);
+			this.setState({
+				gifs: response.data
+			})
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+	}
+
+	showMeGifs(gifs) {
+
 	}
 
 	render() {
 		return (
-		<div>
+		<div className="content">
 			<MainMenu />
+			<Search />
 			<div className="main">
+				{console.log('ahhhh!', this.state.gifs)}
+          {this.state.gifs.map((gif, index) => {
+            return <img src={gif.images.downsized_large.url} key={index} />
 
+          })
+          }
 			</div>
 		</div>)
 	}
 }
+
+				// {this.state.gifs.map((gif, i) => {
+				// 	<img src={gif.images.downsized_large.url} key={i} />
+				// })}
