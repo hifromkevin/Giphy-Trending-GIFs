72aedfb320eb0c83e04fef93a8cbc627dcab8c73
Kevin Lam <lam.kevin86@gmail.com>
2018-11-17 10:31:29 -0800

    Modifies logo style

--- client/src/components/MainMenu.jsx
+++ client/src/components/MainMenu.jsx
@@ -3,7 +3,7 @@ import FontAwesome from 'react-fontawesome';
 
 const MainMenu = () => (
   <div className="menu">
-		<h1 className="menu__logo"><FontAwesome name="file" /> {'{'}not{'}'}GIPHY</h1>
+		<h1 className="menu__logo"><span className="menu__logo__icon"><FontAwesome name="file" /></span> {'{'}not{'}'}GIPHY</h1>
   </div>
 );
 
